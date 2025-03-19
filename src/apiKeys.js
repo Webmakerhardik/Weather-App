77c8ee97642f9e4f8fa15b55eee424511f168547
Hardik Gupta <hardik.21scse1010767@galgotiasuniversity.edu.in>
2025-03-19 11:35:10 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ae03a2533d4d0c53f4ab05ceeb78c288",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
